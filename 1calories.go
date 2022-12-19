d2a8f2fd160d50b853a319d9e0688eec66d95e64
Armin Haghi <armin@haghilabs.com>
2022-12-19 14:33:33 +0800

    updated for part 2 of day 1 to include top N elves

--- 1calories.go
+++ 1calories.go
@@ -1,27 +1,31 @@
+// This program outputs the largest sum of integer
+// elements that are separated by an empty line.
+//
+// To do so it uses two variables:
+// - currentSum tracks the sum of the current group of elements
+// - previousSum tracks the highest sum of elements
+//
+// The main loop goes through every line of the file.
+// - When an empty line is reached, it compares the two sums and updates
+//   previousSum to the highest value of both, then resets currentSum to 0
+//   for the next iteration.
+// - When a number is in the next line, it gets added to currentSum
+
 package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
 )
 
-func main() {
-	// This program outputs the largest sum of integer
-	// elements that are separated by an empty line.
-	//
-	// To do so it uses two variables:
-	// - currentSum tracks the sum of the current group of elements
-	// - previousSum tracks the highest sum of elements
-	//
-	// The main loop goes through every line of the file.
-	// - When an empty line is reached, it compares the two sums and updates
-	//   previousSum to the highest value of both, then resets currentSum to 0
-	//   for the next iteration.
-	// - When a number is in the next line, it gets added to currentSum
+// func printSliceElements
 
-	f, err := os.Open("calories.txt")
+func main() {
+
+	f, err := os.Open("1calories.txt")
 
 	if err != nil {
 		log.Fatal(err)
@@ -31,16 +35,28 @@ func main() {
 
 	scanner := bufio.NewScanner(f)
 
-	var previousSum int64 = 0
+	var topN int = 3
+	topSums := make([]int64, topN)
 	var currentSum int64 = 0
+	var calorieTotals int64
 
 	for scanner.Scan() {
 
 		currentLine := scanner.Text()
 		if currentLine == "" {
-			if previousSum < currentSum {
-				previousSum = currentSum
+			for i := range topSums {
+				if topSums[i] < currentSum {
+					if i == 0 {
+						topSums = append([]int64{currentSum}, topSums[:topN-1]...)
+					} else if i < topN {
+						topSums = append(topSums[:i], append([]int64{currentSum}, topSums[i:topN-1]...)...)
+					} else {
+						topSums = append(topSums[:topN-1], []int64{currentSum}...)
+					}
+					break
+				}
 			}
+
 			currentSum = 0
 		} else {
 			currentLineInt, _ := strconv.ParseInt(currentLine, 0, 64)
@@ -48,7 +64,10 @@ func main() {
 		}
 	}
 
-	println(previousSum)
+	for j := range topSums {
+		calorieTotals += topSums[j]
+	}
+	fmt.Println(calorieTotals)
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
