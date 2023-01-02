e525658ccea71756996fb551638557a16925a740
Armin Haghi <armin@haghilabs.com>
2023-01-02 17:51:15 +0800

    updated duplicate function to search two arbitrary strings

--- day3/backpack2.js
+++ day3/backpack2.js
@@ -1,8 +1,6 @@
-// 1. parse each line 
-// 2. find length / 2 per line = halflenght
-// 3. split each line at halflenght
-// 4. loop for each letter in str1 and str2 until duplicate is found
-// 5. convert to priority a-z 1 to 26 A-Z to 27-52
+// 1. parse three lines line 
+// 2. find duplicates in entries
+// 3. convert to priority a-z 1 to 26 A-Z to 27-52
 let totalPrio = 0;
 let inputFile = 'input.txt';
 
@@ -11,15 +9,12 @@ const fs = require('fs');
 const data = fs.readFileSync(inputFile, 'UTF-8');
 const lines = data.split(/\r?\n/);
 
-function find_duplicate_letter(str) {
-    let split_lenght = str.length / 2;
-    let str1 = str.substring(0, split_lenght);
-    let str2 = str.substring(split_lenght, split_lenght * 2);
-    for (var i = 0; i < split_lenght; i++) {
+function find_duplicate_letter(str1, str2) {
+    for (var i = 0; i < str2.lenght; i++) {
         let pos = void 0;
-        pos = str2.indexOf(str1[i]);
+        pos = str1.indexOf(str2[i]);
         if (pos > -1) {
-            return str2[pos];
+            return str1[pos];
         }
     }
 }
