4cd7bc217dfda8fb7f36bdc4f055b85ca3b1c53b
Armin Haghi <armin@haghilabs.com>
2023-01-02 20:43:11 +0800

    solved day 3 pt 2

--- day3/backpack2.js
+++ day3/backpack2.js
@@ -2,7 +2,8 @@
 // 2. find duplicates in entries
 // 3. convert to priority a-z 1 to 26 A-Z to 27-52
 let totalPrio = 0;
-let inputFile = 'input.txt';
+const inputFile = 'input.txt';
+const groupSize = 3;
 
 const fs = require('fs');
 
@@ -10,13 +11,15 @@ const data = fs.readFileSync(inputFile, 'UTF-8');
 const lines = data.split(/\r?\n/);
 
 function find_duplicate_letter(str1, str2) {
-    for (var i = 0; i < str2.lenght; i++) {
-        let pos = void 0;
+    let reply = "";
+    for (var i = 0; i < str2.length; i++) {
+        let pos = 0;
         pos = str1.indexOf(str2[i]);
         if (pos > -1) {
-            return str1[pos];
-        }
+            reply = reply + str1[pos];
+        } 
     }
+    return reply;
 }
 
 function convert_letter_to_priority(str) {
@@ -30,11 +33,21 @@ function convert_letter_to_priority(str) {
     return prio;
 }
 
-lines.forEach(line => {
-    let str = find_duplicate_letter(line);
-    let prio = convert_letter_to_priority(str);
-    console.log(str, prio);
-    totalPrio = totalPrio + prio;
+let totalLines = lines.length -1 ;
+let linesInGroup = [];
+
+lines.forEach((line, lineNumber) => {
+    linesInGroup.push(line);
+    if ((linesInGroup.length == groupSize) || (lineNumber == totalLines)) {
+        console.log(linesInGroup, lineNumber, totalLines);
+
+        let firstMatch = find_duplicate_letter(linesInGroup[0], linesInGroup[1]);
+        let groupMatch = find_duplicate_letter(firstMatch, linesInGroup[2]);
+        console.log(groupMatch);
+        let prio = convert_letter_to_priority(groupMatch);
+        totalPrio = totalPrio + prio;
+        linesInGroup = [];
+    } 
   })
 
   console.log("Total =", totalPrio);
